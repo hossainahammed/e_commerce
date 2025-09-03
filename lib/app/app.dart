e135febf55391ba5faac55f78dc77034861a3f35
hossainahammed <hossainahammed627@gmail.com>
2025-09-04 01:42:11 +0600

    Firebase & Crashlyticsetup

---
+++ lib/app/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CraftyBay extends StatelessWidget {
+  const CraftyBay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp();
+  }
+}
